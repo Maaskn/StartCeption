20dbf6a099468799e152697b81a5a26fbb21120b
Max Klebe <maaskn@gmail.com>
2015-10-14 15:12:12 +0200

    Buildpath changes

---
+++ StartCeption/src/com/startception/model/DatabaseHandler.java
@@ -0,0 +1,17 @@
+package com.startception.model;
+
+public class DatabaseHandler {
+
+	public DatabaseHandler() {	
+	}
+	
+	public boolean findClient() {
+		return false;
+	}
+	
+	public boolean registerClient() {
+		return false;
+	}
+	
+	
+}
