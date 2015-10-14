271cfa4cc2d3259055521c13abea7f5897eef3d2
Max Klebe <maaskn@gmail.com>
2015-10-14 15:00:15 +0200

    Index html och Client JPA Entity

---
+++ StartCeption/src/com/startception/model/Client.java
@@ -0,0 +1,40 @@
+package com.startception.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the CLIENT database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Client.findAll", query="SELECT c FROM Client c")
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String email;
+
+	private String password;
+
+	public Client() {
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
